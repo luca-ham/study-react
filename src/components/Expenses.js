2ea027cf613447cae166ecb6f714cc366b9d4d14
luca <gyeongjae.h.dev@gmail.com>
2023-12-10 23:04:26 +0900

    :recycle: Change props name

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,24 +5,24 @@ function Expenses(props) {
   return (
     <div className="expenses">
       <ExpenseItem
-        title={props.data[0].title}
-        amount={props.data[0].amount}
-        date={props.data[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       />
       <ExpenseItem
-        title={props.data[1].title}
-        amount={props.data[1].amount}
-        date={props.data[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       />
       <ExpenseItem
-        title={props.data[2].title}
-        amount={props.data[2].amount}
-        date={props.data[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       />
       <ExpenseItem
-        title={props.data[3].title}
-        amount={props.data[3].amount}
-        date={props.data[3].date}
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
       />
     </div>
   );
