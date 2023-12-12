6dcc41278ae3b896c2004ee0b9095833bf26f4ef
luca <gyeongjae.h.dev@gmail.com>
2023-12-12 11:30:33 +0900

    :rewind: Revert to 9923b054ca0551d22a6e1c92dec2c23574ed7e3f

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import componentsImage from './assets/images/components.png';
-import stateImage from './assets/images/state.png';
-import eventsImage from './assets/images/events.png';
-import Header from "./components/Header/Header";
-import Concept from "./components/Concept/Concept";
+import React from "react";
 
-<<<<<<< HEAD
-const concepts = [
-  {
-    title: 'Components',
-    image: componentsImage,
-    description:
-      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
-  },
-  {
-    title: 'State',
-    image: stateImage,
-    description:
-      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
-  },
-  {
-    title: 'Events',
-    image: eventsImage,
-    description:
-      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
-  },
-];
-=======
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -55,29 +29,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
->>>>>>> parent of 9923b05 (:recycle: 화살표 함수로 수정)
 
-function App() {
   return (
     <div>
-      <Header/>
-      <ul id="concepts">
-        <Concept
-          image={concepts[0].image}
-          title={concepts[0].title}
-          description={concepts[0].description}
-        />
-        <Concept
-          image={concepts[1].image}
-          title={concepts[1].title}
-          description={concepts[1].description}
-        />
-        <Concept
-          image={concepts[2].image}
-          title={concepts[2].title}
-          description={concepts[2].description}
-        />
-      </ul>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
